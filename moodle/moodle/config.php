036bbd6455b38e6a1b2f048ad5e0fd625e1d69d8
unknown <marcoschags@hotmail.com>
2016-04-19 21:33:19 -0300

    Novo instalador-3

---
+++ moodle/moodle/config.php
@@ -0,0 +1,33 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = '';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+);
+
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+    $CFG->wwwroot   = 'https://' . $_SERVER['HTTP_HOST'] . '/moodle';
+} else {
+    $CFG->wwwroot   = 'http://' . $_SERVER['HTTP_HOST'] . '/moodle';
+};
+$CFG->dataroot  = 'C:\\xampp\\moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
